09aef0ce4db98dbac7d4fe041ac0293a1a9450cf
Ray Guillotte <rayguillotte@Rays-Air.fios-router.home>
2019-07-28 23:45:44 -0400

    test

--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 
 //import any components needed
 
 //Import your array data to from the provided data file
+import { specials } from "../../../data";
 
 const Specials = () => {
   // STEP 2 - add the imported data to state
+  const [specialsState, setSpecialsState] = useState(specials);
 
   return (
     <div>
